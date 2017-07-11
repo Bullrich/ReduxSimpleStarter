e37f86226f5ed51f7d14959cf01c74747907d388
Javier Bullrich <javierbullrich@gmail.com>
2017-07-11 02:17:55 -0300

    Finished video 26

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
 import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyDj6qT1KFIth5IwY6E-jZfjDHLhunmGPJ0';
+const API_KEY = 'API_KEY';
 
 // Create a new component
-const App = () => {
-    return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'mgsv swap model'}, (videos) => {
+            this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
 // Put component on the DOM
