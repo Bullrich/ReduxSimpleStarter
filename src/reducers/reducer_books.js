6fc2e32f565cb1b727bfb6bb57c7ab40463de2dc
Javier Bullrich <javierbullrich@gmail.com>
2017-07-12 01:37:36 -0300

    Finish lecture 40

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+    return [
+        {title: 'Javascript, the good parts!'},
+        {title: 'Harry potter'},
+        {title: 'The Dark Towah'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
