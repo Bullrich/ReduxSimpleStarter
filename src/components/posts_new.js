cdb2ee0aea2ce83dba25b9be52e5e035805d8efb
Javier Bullrich <javierbullrich@gmail.com>
2017-07-24 23:50:39 -0300

    Finished lecture 133

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
-class PostsNew extends Component{
-    render(){
-        return(
+class PostsNew extends Component {
+    renderTitleField(field) {
+        return (
             <div>
-                PostsNew!
+                <input
+                    type="text"
+                    {...field.input}
+                />
             </div>
+        );
+    }
+
+    render() {
+        return (
+            <form>
+                <Field
+                    name="title"
+                    component={this.renderTitleField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({ // it needs a function for configuration
+    form: 'PostsNewForm' // the str has to be unique
+})(PostsNew);
