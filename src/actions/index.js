feeb930e542da2d409eab62585ce376e708997fb
Javier Bullrich <javierbullrich@gmail.com>
2017-07-26 22:44:33 -0300

    Finished lecture 150, section 9

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
 export const CREATE_POST = 'create_post';
+export const FETCH_POST = 'fetch_post';
+export const DELETE_POST = 'delete_post';
 
 const API_URL = 'http://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=PAPERCLIP1234';
@@ -22,5 +24,24 @@ export function createPost(values, callback) {
     return {
         type: CREATE_POST,
         payload: request
+    };
+}
+
+export function fetchPost(id) {
+    const request = axios.get(`${API_URL}/posts/${id}${API_KEY}`);
+
+    return {
+        type: FETCH_POST,
+        payload: request
+    };
+}
+
+export function deletePost(id, callback){
+    const request = axios.delete(`${API_URL}/posts/${id}${API_KEY}`)
+        .then(() => callback());
+
+    return{
+        type: DELETE_POST,
+        payload: id // no need to pass the whole post if it's being deleted
     };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,11 +1,22 @@
 import _ from 'lodash';
-import {FETCH_POSTS} from "../actions/index";
+import {FETCH_POSTS, FETCH_POST, DELETE_POST} from "../actions/index";
 
 export default function (state = {}, action) {
     switch (action.type) {
         case FETCH_POSTS:
             console.log(action.payload.data); // [post1, post2]
-            return _.mapKeys(action.payload.data, 'id');
+            return _.mapKeys(action.payload.data, 'id'); // {id1:post1,id2:post2}
+        case FETCH_POST:
+            // E5
+            // const post = action.payload.data;
+            // const newState = {...state };
+            // newState[post.id] = post;
+            // return newState;
+
+            // E6
+            return {...state, [action.payload.data.id]: action.payload.data};
+        case DELETE_POST:
+            return _.omit(state, action.payload);
         default:
             return state;
     }
